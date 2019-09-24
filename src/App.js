d39bafac96990737d560dfdf2e313157f81ffad8
Franklin Jingyuan Xu <FranklinXu@xujingyuandeMacBook-Pro.local>
2019-09-24 12:32:14 +0800

    finshed this

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <h1>Keine Ahnung!</h1>
+        <p>I am doing great</p> 
+        <img alt="brilliance" src="http://www.wiseinkblog.com/wp-content/uploads/2012/03/shutterstock_97473626.jpg" width="300" height="200"/>
+        <a href="https://en.wikipedia.org/wiki/The_Home_Depot">My Profile</a>
+        <h2>about me</h2>
+        <p>I like to hang out with you!!!!!!!</p>
+        <p><strong>nickNAME:</strong>: hello</p>
+        <h1>Hobbies:</h1>
+        <ul>
+            <li>Fishng</li>
+            <li>Slllll</li>
+        </ul>
+    <button type="button">Connect</button>
+    </body>
     </div>
   );
 }
